2cbb4e5bd6ff918eacc13d2d8b60feb268e1100a
DUSHIME MUDAHERA RICHARD <mudaherarich@gmail.com>
2023-06-02 11:20:32 +0300

    landing page

--- FRB_Attendance_Monitoring/FRB_Attendance_Monitoring/settings.py
+++ FRB_Attendance_Monitoring/FRB_Attendance_Monitoring/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'staticfiles/')
+]
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
