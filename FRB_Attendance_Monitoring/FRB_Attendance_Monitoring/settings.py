32958b34944ef783f8cd02b8fed9968a2bd955c5
Daniel Ntege <danientege785@gmail.com>
2023-06-07 11:26:58 +0000

    Accounts app, Login Page for the staff

--- FRB_Attendance_Monitoring/FRB_Attendance_Monitoring/settings.py
+++ FRB_Attendance_Monitoring/FRB_Attendance_Monitoring/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'students.apps.StudentsConfig',
     'staff.apps.StaffConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
