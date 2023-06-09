5b9e347e92294f29a964dedd3d9b3cd0f71b2624
Daniel Ntege <danientege785@gmail.com>
2023-06-09 12:58:07 +0000

    Accounts

---
+++ FRB_Attendance_Monitoring/accounts/middleware.py
@@ -0,0 +1,38 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib import messages
+
+
+class AccountCheckMiddleWare(MiddlewareMixin):
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        modulename = view_func.__module__
+        user = request.user  # Who is the current user ?
+        if user.is_authenticated:
+            if user.user_type == '1':  # Admin
+                if modulename == 'students.views':
+                    error = True
+                    if request.path == reverse('fetch_ballot'):
+                        pass
+                    else:
+                        messages.error(
+                            request, "You do not have access to this resource")
+                        return redirect(reverse('adminDashboard'))
+            elif user.user_type == '2':  # Student
+                if modulename == 'staff.views':
+                    messages.error(
+                        request, "You do not have access to this resource")
+                    return redirect(reverse('voterDashboard'))
+            else:  # None of the aforementioned ? Please take the user to login page
+                return redirect(reverse('account_login'))
+        else:
+            # If the path is login or has anything to do with authentication, pass
+            if request.path == reverse('account_login') or request.path == reverse('account_register') or modulename == 'django.contrib.auth.views' or request.path == reverse('account_login'):
+                pass
+            elif modulename == 'staff.views' or modulename == 'students.views':
+                # If visitor tries to access administrator or voters functions
+                messages.error(
+                    request, "You need to be logged in to perform this operation")
+                return redirect(reverse('account_login'))
+            else:
+                return redirect(reverse('account_login'))
--- FRB_Attendance_Monitoring/accounts/views.py
+++ FRB_Attendance_Monitoring/accounts/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from .email_backend import EmailBackend
+from django.contrib import messages
+from .forms import CustomUserForm
+from voting.forms import studentForm
+from django.contrib.auth import login, logout
 # Create your views here.
+
+
+def account_login(request):
+    if request.user.is_authenticated:
+        if request.user.user_type == '1':
+            return redirect(reverse("staffDashboard"))
+        else:
+            return redirect(reverse("studentDashboard"))
+
+    context = {}
+    if request.method == 'POST':
+        user = EmailBackend.authenticate(request, username=request.POST.get(
+            'email'), password=request.POST.get('password'))
+        if user != None:
+            login(request, user)
+            if user.user_type == '1':
+                return redirect(reverse("staffDashboard"))
+            else:
+                return redirect(reverse("studentDashboard"))
+        else:
+            messages.error(request, "Invalid details")
+            return redirect("/")
+
+    return render(request, "students/login.html", context)
+
+
+def account_register(request):
+    userForm = CustomUserForm(request.POST or None)
+    studentForm = studentForm(request.POST or None)
+    context = {
+        'form1': userForm,
+        'form2': studentForm
+    }
+    if request.method == 'POST':
+        if userForm.is_valid() and studentForm.is_valid():
+            user = userForm.save(commit=False)
+            student = studentForm.save(commit=False)
+            student.admin = user
+            user.save()
+            student.save()
+            messages.success(request, "Account created. You can login now!")
+            return redirect(reverse('account_login'))
+        else:
+            messages.error(request, "Provided data failed validation")
+            # return account_login(request)
+    return render(request, "stdent/reg.html", context)
+
+
+def account_logout(request):
+    user = request.user
+    if user.is_authenticated:
+        logout(request)
+        messages.success(request, "Thank you for visiting us!")
+    else:
+        messages.error(
+            request, "You need to be logged in to perform this action")
+
+    return redirect(reverse("account_login"))
