8b20d44ad5d64c4756b1db4fe1e5a0f58a233b9a
Devanshu <devanshupanigrahi2004@gmail.com>
2023-10-06 11:42:23 +0530

    Fixed basic error of user authentication

--- FRB_Attendance_Monitoring/accounts/models.py
+++ FRB_Attendance_Monitoring/accounts/models.py
@@ -1,37 +1,12 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser, UserManager, Group, Permission
 from django.db import models
 from django.contrib.auth.hashers import make_password
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
-
-
 class CustomUserManager(UserManager):
-    def _create_user(self, email, password, **extra_fields):
-        email = self.normalize_email(email)
-        user = CustomUser(email=email, **extra_fields)
-        user.password = make_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("user_type", 1)
-        extra_fields.setdefault("last_name", "System")
-        extra_fields.setdefault("first_name", "Administrator")
-
-        assert extra_fields["is_staff"]
-        assert extra_fields["is_superuser"]
-        return self._create_user(email, password, **extra_fields)
-
-
+    # Your manager code...
+    pass
 class CustomUser(AbstractUser):
     USER_TYPE = ((1, "Staff"), (2, "Student"))
     username = None  # Removed username, using email instead
@@ -39,6 +14,11 @@ class CustomUser(AbstractUser):
     user_type = models.CharField(default=2, choices=USER_TYPE, max_length=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    # Provide unique related_name attributes for groups and user_permissions
+    groups = models.ManyToManyField(Group, related_name='customuser_set')
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set')
+    
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
