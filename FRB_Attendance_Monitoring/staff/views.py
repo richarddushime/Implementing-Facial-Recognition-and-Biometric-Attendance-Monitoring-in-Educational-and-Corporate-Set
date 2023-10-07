96edc381aff08b0877bca5b831eccc48b1f84230
richarddushime <mudaherarich@gmail.com>
2023-10-07 21:06:02 +0300

    Admin Pannel UI

--- FRB_Attendance_Monitoring/staff/views.py
+++ FRB_Attendance_Monitoring/staff/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def staff_dashboard(request):
+    return render(request, 'staff_dashboard.html')
