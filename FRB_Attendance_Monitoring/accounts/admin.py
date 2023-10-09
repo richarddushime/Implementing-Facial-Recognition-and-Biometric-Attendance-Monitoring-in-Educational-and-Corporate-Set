49a42923683f2fdf35c871e4b5eff4a676d67abf
Samarth <72240452+Quinos2003@users.noreply.github.com>
2023-10-10 00:28:11 +0530

    Update admin.py with CustomUser Model

--- FRB_Attendance_Monitoring/accounts/admin.py
+++ FRB_Attendance_Monitoring/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import *
+# Register your models here.
+
 
 # Register your models here.
+# admin.site.register(CustomUserManager)
+admin.site.register(CustomUser)
