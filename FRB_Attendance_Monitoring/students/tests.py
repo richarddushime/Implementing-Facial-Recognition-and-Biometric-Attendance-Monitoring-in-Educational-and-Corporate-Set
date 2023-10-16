242ab6a3d8412ee35e0546228ddb1f2c48b14bad
jovialumwari <umwarijovial@gmail.com>
2023-10-16 22:30:14 +0300

    Students Application Unit Tests

--- FRB_Attendance_Monitoring/students/tests.py
+++ FRB_Attendance_Monitoring/students/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+class IndexViewTest(TestCase):
+    def test_index_view(self):
+        # Use the reverse function to get the URL for the 'index' view
+        url = reverse('index')
+
+        # Perform a GET request to the 'index' view
+        response = self.client.get(url)
+
+        # Check if the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the 'home.html' template is used in the response
+        self.assertTemplateUsed(response, 'home.html')
