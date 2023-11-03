619bf9ba7d6578515cd42ff53176a9f20c654698
Olumide Samuel Damilare <nou223135065@nou.edu.ng>
2023-11-03 17:45:17 +0100

    updated views.py to handle admin staff registration logic

--- FRB_Attendance_Monitoring/staff/views.py
+++ FRB_Attendance_Monitoring/staff/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AdminStaffRegistrationForm
+
+def adminstaff_registration(request):
+    if request.method == 'POST':
+        form = AdminStaffRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('staff_dashboard')
+    else:
+        form = AdminStaffRegistrationForm()
+    context = {'form': form}
+    return render(request, 'adminstaff_registration.html', context)
 
 # Create your views here.
 def index(request):
